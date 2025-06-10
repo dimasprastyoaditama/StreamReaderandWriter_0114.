7ca4ed325dda82869596c8cd4abd63840bbdb887
dimasprastyoaditama <prastyod647@gmail.com>
2025-06-10 17:55:06 +0700

    menampilkan

--- ioFile.cpp
+++ ioFile.cpp
@@ -32,6 +32,10 @@ int main() {
     {
         //melakukan perulangan setiap baris
         while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
 
 
 }
